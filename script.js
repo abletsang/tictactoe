a94265801ffc91ba6cd55ec140ed672301d5e401
Able Tsang <abletsang@gmail.com>
2017-05-19 17:50:25 -0700

    instead of rendering background images when clicked, change to inserting x or o as text

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("click", function() {
 		if (turn === true) {
 			if (availableMoves[this.id] != -1) {
-				this.classList.add(player);
+				this.textContent = player;
 				turn = false;
 				availableMoves[this.id] = -1;
 				playerMoves.push(this.id);
@@ -93,7 +93,7 @@ function computerPlay() {
 	var temp = Math.floor(Math.random() * 8);
 	if (availableMoves[temp] != -1) {
 		console.log(temp);
-		squares[temp].classList.add(compPlayer);
+		squares[temp].textContent = compPlayer;
 		turn = true;
 		return;
 	} else {
@@ -105,8 +105,7 @@ function computerPlay() {
 function reset() {
 	availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 	for (var i = 0; i < squares.length; i++) {
-		squares[i].classList.remove("o");
-		squares[i].classList.remove("x");
+		squares[i].textContent = "";
 	}
 	playerMoves = [];
 	compMoves = [];
